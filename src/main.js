04218360b82d107ea5db4d13c4bc7c67184f1301
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2020-11-02 08:15:44 +0100

    initial commit, basic functionality

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import store from "./store";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    name: "LigFinderInfoscreen",
+    vuetify,
+    store,
+    render: h => h(App)
+}).$mount("#app");
